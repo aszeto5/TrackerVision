68f221ad0a45d94ca576c9b6c2456a07521bed9b
Adrian Duong Szeto <adrianduongszeto@gmail.com>
2022-08-17 21:11:48 -0700

    request urls for tokens

--- trackervision/server/server.js
+++ trackervision/server/server.js
@@ -8,10 +8,31 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
+const sampleRequestToken = '0b853d9598b16cd1628f6dd717a4152d667d3c32'
+
 //call for session id: https://api.themoviedb.org/3/authentication/session/new?api_key=93916c935979931b0ffa32a131a1cf5e
 
 const API_KEY = '93916c935979931b0ffa32a131a1cf5e'
-//exmample api request: https://api.themoviedb.org/3/movie/550?api_key=93916c935979931b0ffa32a131a1cf5e
+const request_token = requestToken()
+//1. generate request token, 2. TMDb approve token, 3. generate access token
+
+//create request token (60mins duration)
+const requestToken = async () => {
+     `https://api.themoviedb.org/3/authentication/token/new?api_key=${API_KEY}`
+}
+
+//approve redirect
+const approveRequest = async () => {
+    await axios.get (`https://www.themoviedb.org/auth/access?request_token=${request_token}?redirect_to=http://www.yourapp.com/approved`)
+}
+approveRequest();
+
+//create access token/session (need request token)
+const session = `https://api.themoviedb.org/3/authentication/session/new?api_key=${API_KEY}`
+
+//delete access token
+
+
 
 //sign up
 app.post('/signup', async (req,res) => {
@@ -29,6 +50,31 @@ app.post('/signup', async (req,res) => {
     } catch(error) {
         console.log(error)
 
+        res.status(500).json({message: error})
+    }
+})
+
+//login
+
+app.post('/login', async (req,res) => {
+    try {
+        const { username, password, request_token } = req.body
+        const client = connect(API_KEY)
+
+        if (!users.length) return res.json(400).json({ message: 'User does not exist'})
+
+        const success = await bcrypt.compare(password, users[0].hashedPassword)
+        const token = client.createUserToken(users[0].id)
+        const confirmedName = users[0].name
+        const userId = users[0].id
+
+        if (success) {
+            res.status(200).json({ token, username: confirmedName, userId })
+        } else {
+            res.status(500).json({ message: 'Login failed'})
+        }
+    } catch (error) {
+        console.log(error)
         res.status(500).json({message: error})
     }
 })
